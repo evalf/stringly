c7d620cccc36e608b35321f6513487d5c948aae4
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2023-10-25 15:06:04 +0200

    add context to SerializationError

--- stringly/error.py
+++ stringly/error.py
@@ -1,3 +1,7 @@
 class StringlyError(Exception): pass
-class SerializationError(StringlyError): pass
+class SerializationError(StringlyError):
+    def __init__(self, ctx, msg):
+        if ctx:
+            msg = f'in {ctx}: {msg}'
+        super().__init__(msg)
 class ImportFunctionError(StringlyError): pass
--- stringly/serializer.py
+++ stringly/serializer.py
@@ -78,19 +78,22 @@ def get(t: typing.Any) -> proto.Serializer[typing.Any]:
     raise ValueError(f'unsupported type: {t}')
 
 
-def _assert_isinstance(v, *types):
+def _assert_isinstance(ctx, v, *types):
     if not isinstance(v, types):
-        raise error.SerializationError(f'{v} <{type(v).__qualname__}> is not an instance of {" or ".join(T.__qualname__ for T in types)}')
+        raise error.SerializationError(ctx, f'{v} <{type(v).__qualname__}> is not an instance of {" or ".join(T.__qualname__ for T in types)}')
 
 
 class Custom(typing.Generic[T]):
     def __init__(self, C: proto.Custom[T]) -> None:
         self.C = C
 
-    def loads(self, s: str) -> T:
-        return self.C.__stringly_loads__(s)
+    def loads(self, s: str, ctx: str = '') -> T:
+        try:
+            return self.C.__stringly_loads__(s)
+        except Exception as e:
+            raise error.SerializationError(ctx, f'failed to deserialize {s!r}: {e}') from None
 
-    def dumps(self, v: T) -> str:
+    def dumps(self, v: T, ctx: str = '') -> str:
          return self.C.__stringly_dumps__(v)
 
     def __str__(self) -> str:
@@ -98,14 +101,14 @@ class Custom(typing.Generic[T]):
 
 
 class Boolean:
-    def loads(self, s: str) -> bool:
+    def loads(self, s: str, ctx: str = '') -> bool:
         v = dict(true=True, yes=True, false=False, no=False).get(s.lower())
         if v is None:
-            raise error.SerializationError(f'invalid boolean value {s!r}')
+            raise error.SerializationError(ctx, f'invalid boolean value {s!r}')
         return v
 
-    def dumps(self, v: bool) -> str:
-        _assert_isinstance(v, bool)
+    def dumps(self, v: bool, ctx: str = '') -> str:
+        _assert_isinstance(ctx, v, bool)
         return bool.__str__(v)
 
     def __str__(self) -> str:
@@ -118,15 +121,15 @@ class Native:
         self.alt = alt
         self.trim = trim
 
-    def loads(self, s: str) -> int:
+    def loads(self, s: str, ctx: str = '') -> int:
         try:
             v = self.T(s)
         except Exception as e:
-            raise error.SerializationError(e)
+            raise error.SerializationError(ctx, e) from None
         return v
 
-    def dumps(self, v):
-        _assert_isinstance(v, self.T, *self.alt)
+    def dumps(self, v, ctx: str = ''):
+        _assert_isinstance(ctx, v, self.T, *self.alt)
         s = str(self.T(v))
         for prefix, suffix in self.trim:
             if s.startswith(prefix) and s.endswith(suffix):
@@ -141,12 +144,12 @@ class UniformTuple(typing.Generic[T]):
     def __init__(self, itemserializer: proto.Serializer[T]) -> None:
         self.itemserializer = itemserializer
 
-    def loads(self, s: str) -> typing.Tuple[T,...]:
+    def loads(self, s: str, ctx: str = '') -> typing.Tuple[T,...]:
         parts = util.safesplit(s, ',')
-        return tuple(self.itemserializer.loads(util.unprotect(si)) for si in parts)
+        return tuple(self.itemserializer.loads(util.unprotect(si), f'{ctx}:{i+1}') for i, si in enumerate(parts))
 
-    def dumps(self, v: typing.Tuple[T, ...]) -> str:
-        return ','.join(util.protect_regex(self.itemserializer.dumps(vi), ',') or '{}' for vi in v)
+    def dumps(self, v: typing.Tuple[T, ...], ctx: str = '') -> str:
+        return ','.join(util.protect_regex(self.itemserializer.dumps(vi, f'{ctx}:{i+1}'), ',') or '{}' for i, vi in enumerate(v))
 
     def __str__(self) -> str:
         return f'typing.Tuple[{self.itemserializer}, ...]'
@@ -156,16 +159,16 @@ class PluriformTuple:
     def __init__(self, itemserializers: typing.Tuple[proto.Serializer[typing.Any], ...]) -> None:
         self.itemserializers = itemserializers
 
-    def loads(self, s: str) -> typing.Tuple[typing.Any, ...]:
+    def loads(self, s: str, ctx: str = '') -> typing.Tuple[typing.Any, ...]:
         parts = util.safesplit(s, ',')
         if len(self.itemserializers) == len(parts):
-            return tuple(zi.loads(util.unprotect(si)) for zi, si in zip(self.itemserializers, parts))
-        raise error.SerializationError('tuple has incorrect length')
+            return tuple(zi.loads(util.unprotect(si), f'{ctx}:{i+1}') for i, zi, si in zip(range(len(parts)), self.itemserializers, parts))
+        raise error.SerializationError(ctx, 'tuple has incorrect length')
 
-    def dumps(self, v: typing.Tuple[typing.Any, ...]) -> str:
+    def dumps(self, v: typing.Tuple[typing.Any, ...], ctx: str = '') -> str:
         if len(self.itemserializers) == len(v):
-            return ','.join(util.protect_regex(zi.dumps(vi), ',') or '{}' for zi, vi in zip(self.itemserializers, v))
-        raise error.SerializationError('tuple has incorrect length')
+            return ','.join(util.protect_regex(zi.dumps(vi, f'{ctx}:{i+1}'), ',') or '{}' for i, zi, vi in zip(range(len(v)), self.itemserializers, v))
+        raise error.SerializationError(ctx, 'tuple has incorrect length')
 
     def __str__(self) -> str:
         return f'typing.Tuple[{", ".join(map(str, self.itemserializers))}]'
@@ -176,18 +179,18 @@ class Dict(typing.Generic[K, V]):
         self.keyserializer = keyserializer
         self.valueserializer = valueserializer
 
-    def loads(self, s: str) -> typing.Dict[K, V]:
+    def loads(self, s: str, ctx: str = '') -> typing.Dict[K, V]:
         v: typing.Dict[K, V] = {}
         for si in util.safesplit(s, ','):
             parts = util.safesplit(si, '=', 1)
             if len(parts) != 2:
-                raise error.SerializationError('missing value')
+                raise error.SerializationError(ctx, 'missing value')
             key, value = map(util.unprotect, parts)
-            v[self.keyserializer.loads(key)] = self.valueserializer.loads(value)
+            v[self.keyserializer.loads(key, f'{ctx}:?')] = self.valueserializer.loads(value, f'{ctx}:{key}')
         return v
 
-    def dumps(self, v: typing.Dict[K, V]) -> str:
-        return ','.join(util.protect_regex(self.keyserializer.dumps(vk), ',|=') + '=' + util.protect_regex(self.valueserializer.dumps(vv), ',') for vk, vv in v.items())
+    def dumps(self, v: typing.Dict[K, V], ctx: str = '') -> str:
+        return ','.join(util.protect_regex(self.keyserializer.dumps(vk, f'{ctx}:?'), ',|=') + '=' + util.protect_regex(self.valueserializer.dumps(vv, f'{ctx}:{vk}'), ',') for vk, vv in v.items())
 
     def __str__(self) -> str:
         return f'typing.Dict[{self.keyserializer}, {self.valueserializer}]'
@@ -197,20 +200,20 @@ class Union:
     def __init__(self, serializers: typing.Mapping[str, proto.Serializer[typing.Any]]) -> None:
         self.serializers = serializers
 
-    def loads(self, s: str) -> typing.Any:
+    def loads(self, s: str, ctx: str = '') -> typing.Any:
         name, value = util.splitarg(s)
         if name not in self.serializers:
-            raise error.SerializationError(f'unknown type: {name}')
-        return self.serializers[name].loads(value)
+            raise error.SerializationError(ctx, f'unknown type: {name}')
+        return self.serializers[name].loads(value, f'{ctx}({name})')
 
-    def dumps(self, v: typing.Any) -> str:
+    def dumps(self, v: typing.Any, ctx: str = '') -> str:
         for name, serializer in self.serializers.items():
             try:
-                s = serializer.dumps(v)
+                s = serializer.dumps(v, ctx)
             except error.SerializationError:
                 continue
             return name + util.protect_unconditionally(s) if s else name
-        raise error.SerializationError('failed to find matching serializer')
+        raise error.SerializationError(ctx, 'failed to find matching serializer')
 
     def __str__(self) -> str:
         return f'typing.Union[{", ".join(map(str, self.serializers.values()))}]'
@@ -220,15 +223,15 @@ class Optional(typing.Generic[T]):
     def __init__(self, serializer: proto.Serializer[T]) -> None:
         self.serializer = serializer
 
-    def loads(self, s: str) -> typing.Optional[T]:
+    def loads(self, s: str, ctx: str = '') -> typing.Optional[T]:
         if s == '':
             return None
-        return self.serializer.loads(util.unprotect(s))
+        return self.serializer.loads(util.unprotect(s), ctx)
 
-    def dumps(self, v: typing.Optional[T]) -> str:
+    def dumps(self, v: typing.Optional[T], ctx: str = '') -> str:
         if v is None:
             return ''
-        s = self.serializer.dumps(v)
+        s = self.serializer.dumps(v, ctx)
         return util.protect_unconditionally(s) if s.startswith('{') and s.endswith('}') or not s else s
 
     def __str__(self) -> str:
@@ -240,11 +243,11 @@ class Sequence:
         self.itemserializer = itemserializer
         self.origin = origin
 
-    def loads(self, s: str) -> typing.Any:
-        return self.origin(self.itemserializer.loads(util.unprotect(si)) for si in util.safesplit(s, ','))
+    def loads(self, s: str, ctx: str = '') -> typing.Any:
+        return self.origin(self.itemserializer.loads(util.unprotect(si), f'{ctx}[{i}]') for i, si in enumerate(util.safesplit(s, ',')))
 
-    def dumps(self, v: typing.Any) -> str:
-        return ','.join(util.protect_regex(self.itemserializer.dumps(vi), ',') or '{}' for vi in v)
+    def dumps(self, v: typing.Any, ctx: str = '') -> str:
+        return ','.join(util.protect_regex(self.itemserializer.dumps(vi, f'{ctx}[{i}]'), ',') or '{}' for i, vi in enumerate(v))
 
     def __str__(self) -> str:
         typename = {list: 'typing.List', set: 'typing.Set', frozenset: 'typing.FrozenSet'}[self.origin]
@@ -257,11 +260,14 @@ class Enum(typing.Generic[enumT]):
     def __init__(self, cls: typing.Type[enumT]) -> None:
         self.cls = cls
 
-    def loads(self, s: str) -> enumT:
-        return self.cls.__members__[s]
+    def loads(self, s: str, ctx: str = '') -> enumT:
+        try:
+            return self.cls.__members__[s]
+        except KeyError:
+            raise error.SerializationError(ctx, f'invalid enum value {s!r}') from None
 
-    def dumps(self, v: enumT) -> str:
-        _assert_isinstance(v, self.cls)
+    def dumps(self, v: enumT, ctx: str = '') -> str:
+        _assert_isinstance(ctx, v, self.cls)
         return v.name
 
     def __str__(self) -> str:
@@ -297,7 +303,7 @@ class Generic(typing.Generic[T]):
                 raise Exception(f'invalid function signature: type cannot be inferred for argument {param.name!r}')
             self.serializers.append(get(T))
 
-    def loads(self, s: str) -> T:
+    def loads(self, s: str, ctx: str = '') -> T:
         args = self.defaults.copy()
         if not s:
             pass
@@ -305,7 +311,7 @@ class Generic(typing.Generic[T]):
             if not self.npositional:
                 parts = util.safesplit(s, '=', 1)
                 if len(parts) != 2 or parts[0] != self.argnames[0]:
-                    raise error.SerializationError(f'invalid argument for {self.cls.__name__}: {s}') from None
+                    raise error.SerializationError(ctx, f'invalid argument for {self.cls.__name__}: {s}') from None
                 s = parts[1]
             args[0] = _strarg(util.unprotect(s))
         else:
@@ -317,22 +323,22 @@ class Generic(typing.Generic[T]):
                     try:
                         index = self.argnames.index(name, self.npositional)
                     except ValueError:
-                        raise error.SerializationError(f'invalid argument {name!r}') from None
+                        raise error.SerializationError(ctx, f'invalid argument {name!r}') from None
                     args[index] = _strarg(value)
                 elif index < self.npositional:
                     args[index] = _strarg(util.unprotect(si))
                     index += 1
                 else:
-                    raise error.SerializationError('invalid expression')
+                    raise error.SerializationError(ctx, 'invalid expression')
         for i, arg in enumerate(args):
             if arg is inspect.Parameter.empty:
-                raise error.SerializationError(f'missing mantatory argument {self.argnames[i]!r}')
+                raise error.SerializationError(ctx, f'missing mantatory argument {self.argnames[i]!r}')
             if isinstance(arg, _strarg):
-                args[i] = self.serializers[i].loads(arg.value)
+                args[i] = self.serializers[i].loads(arg.value, f'{ctx}.{self.argnames[i]}')
         return self.cls(*args[:self.npositional], **dict(zip(self.argnames[self.npositional:], args[self.npositional:])))
 
-    def dumps(self, v: T) -> str:
-        _assert_isinstance(v, self.cls)
+    def dumps(self, v: T, ctx: str = '') -> str:
+        _assert_isinstance(ctx, v, self.cls)
         if hasattr(self.cls, '__getnewargs_ex__'):
             args, kwargs = self.cls.__getnewargs_ex__(v) # type: ignore
             assert len(args) + len(kwargs) == len(self.argnames)
@@ -343,8 +349,8 @@ class Generic(typing.Generic[T]):
         elif dataclasses.is_dataclass(self.cls):
             args = tuple(getattr(v, name) for name in self.argnames)
         else:
-            raise error.SerializationError(f'cannot dump {v}')
-        dumps = [serializer.dumps(arg) for serializer, arg in zip(self.serializers, args)]
+            raise error.SerializationError(ctx, f'cannot dump {v}')
+        dumps = [serializer.dumps(arg, f'{ctx}.{argname}') for serializer, arg, argname in zip(self.serializers, args, self.argnames)]
         if len(self.argnames) == 1:
             return util.protect_unbalanced(dumps[0]) or '{}' if self.npositional \
               else util.protect_regex(self.argnames[0], '=') + '=' + util.protect_unbalanced(dumps[0])
